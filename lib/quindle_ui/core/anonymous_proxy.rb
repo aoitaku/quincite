b32a28ee3dc04291201871616b28c511c76937eb
aoitaku <aoitaku@gmail.com>
2015-01-23 05:12:24 +0900

    modify namespace

--- lib/quindle_ui/core/anonymous_proxy.rb
+++ lib/quindle_ui/core/anonymous_proxy.rb
@@ -1,8 +1,11 @@
-module Quindle::AnonymousProxy
+module Quindle
 
-  def anon_proxy(name, sup=BasicObject, &proc)
-    instance_variable_set(:"@#{name}", Class.new(sup, &proc).new)
-    class_eval("def self.#{name}() @#{name} end")
+  module AnonymousProxy
+
+    def anon_proxy(name, sup=BasicObject, &proc)
+      instance_variable_set(:"@#{name}", Class.new(sup, &proc).new)
+      class_eval("def self.#{name}() @#{name} end")
+    end
+
   end
-
 end
